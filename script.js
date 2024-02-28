548a4390e74977ba8701bdd1d47f4fb2d36ae348
Jinu Vijayan <jinuvijayan01@gmail.com>
2024-02-28 19:55:35 +0530

    Change:changed code to not use jsx

--- script.js
+++ script.js
@@ -1,38 +1,24 @@
-const firstPara = "Welcome to the MDN learning area. This set of articles aims to guide complete beginners to web development with all that they need to start coding websites.";
+const h1 = React.createElement("h1",{},"Learn Web Development");
 
-const lastPara = "If you are a complete beginner, web development can be challenging — we will hold your hand and provide enough detail for you to feel comfortable and learn the topics properly. You should feel at home whether you are a student learning web development (on your own or as part of a class), a teacher looking for class materials, a hobbyist, or someone who just wants to understand more about how web technologies work."
+const firstPara = React.createElement("p",{},"Welcome to the MDN learning area. This set of articles aims to guide complete beginners to web development with all that they need to start coding websites.");
 
-const Heading = () => {
-    return(
-        <h1>Learn Web Development</h1>
-    )
-}
+const firstDiv = React.createElement("div",{},firstPara);
 
-const ParaDiv = ({para}) => {
-    return(
-        <div>
-            <p>{para}</p>
-        </div>
-    )
-}
+const firstSpan = React.createElement("span",{},'The aim of this area of MDN is not to take you from "beginner" to "expert" but to take you from "beginner" to "comfortable." From there, you should be able to start making your way, learning from');
 
-const SecondDiv = () => {
-    return(
-        <div>
-            <p>The aim of this area of MDN is not to take you from "beginner" to "expert" but to take you from "beginner" to "comfortable." From there, you should be able to start making your way, learning from <a href="https://developer.mozilla.org/en-US/">the rest of MDN</a>, and other intermediate to advanced resources that assume a lot of previous knowledge.</p>
-        </div>
-    )
-}
+const secondSpan = React.createElement("span",{},', and other intermediate to advanced resources that assume a lot of previous knowledge.');
 
-const Article = () => {
-    return(
-        <article>
-            <Heading/>
-            <ParaDiv para = {firstPara}/>
-            <SecondDiv/>
-            <ParaDiv para = {lastPara}/>
-        </article>
-    )
-}
+const anchor = React.createElement("a",{href:"https://developer.mozilla.org/en-US/"}," the rest of MDN");
 
-ReactDOM.render(<Article/>,document.querySelector("#root"))
+const secondPara = React.createElement("p",{},[firstSpan,anchor,secondSpan]);
+
+const secondDiv = React.createElement("div",{},secondPara);
+
+const thirdPara = React.createElement("p",{},"If you are a complete beginner, web development can be challenging — we will hold your hand and provide enough detail for you to feel comfortable and learn the topics properly. You should feel at home whether you are a student learning web development (on your own or as part of a class), a teacher looking for class materials, a hobbyist, or someone who just wants to understand more about how web technologies work.")
+
+const thirdDiv = React.createElement("div",{},thirdPara);
+
+const div = React.createElement("div",{},[h1,firstDiv,secondDiv,thirdDiv]);
+
+
+ReactDOM.render(div,document.querySelector("#root"))
