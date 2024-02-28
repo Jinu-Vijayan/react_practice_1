4491dfed1333b995b4ccaafc3285a62588705e3d
Jinu Vijayan <jinuvijayan01@gmail.com>
2024-02-28 08:22:14 +0530

    Add:adding component data dynamically through variables

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
+const firstPara = "Welcome to the MDN learning area. This set of articles aims to guide complete beginners to web development with all that they need to start coding websites.";
+
+const lastPara = "If you are a complete beginner, web development can be challenging — we will hold your hand and provide enough detail for you to feel comfortable and learn the topics properly. You should feel at home whether you are a student learning web development (on your own or as part of a class), a teacher looking for class materials, a hobbyist, or someone who just wants to understand more about how web technologies work."
+
 const Heading = () => {
     return(
         <h1>Learn Web Development</h1>
     )
 }
 
-const FirstDiv = () => {
+const ParaDiv = ({para}) => {
     return(
         <div>
-            <p>Welcome to the MDN learning area. This set of articles aims to guide complete beginners to web development with all that they need to start coding websites.</p>
+            <p>{para}</p>
         </div>
     )
 }
@@ -20,21 +24,13 @@ const SecondDiv = () => {
     )
 }
 
-const ThirdDiv = () => {
-    return(
-        <div>
-            <p>If you are a complete beginner, web development can be challenging — we will hold your hand and provide enough detail for you to feel comfortable and learn the topics properly. You should feel at home whether you are a student learning web development (on your own or as part of a class), a teacher looking for class materials, a hobbyist, or someone who just wants to understand more about how web technologies work.</p>
-        </div>
-    )
-}
-
 const Article = () => {
     return(
         <article>
             <Heading/>
-            <FirstDiv/>
+            <ParaDiv para = {firstPara}/>
             <SecondDiv/>
-            <ThirdDiv/>
+            <ParaDiv para = {lastPara}/>
         </article>
     )
 }
